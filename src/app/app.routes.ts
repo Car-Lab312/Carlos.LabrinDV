d41bf51c73cacb5b29555f137ceccd58e8d2e620
Car-Lab312 <carlos.labrin15@gmail.com>
2026-03-10 16:38:11 -0300

    respaldo carlos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
